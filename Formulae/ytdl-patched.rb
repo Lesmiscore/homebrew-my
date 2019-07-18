3ea70c85836c995e7cab937bf3a3d69dadc2218b
nao20010128nao <nao20010128@gmail.com>
2019-07-18 03:39:15 +0000

    ytdl-patched

---
+++ Formulae/ytdl-patched.rb
@@ -0,0 +1,23 @@
+class YtdlPatched < Formula
+  desc "Download YouTube videos from the command-line"
+  homepage "https://ytdl-org.github.io/youtube-dl/"
+  url "https://nao20010128nao.github.io/ytdl-patched/youtube-dl.tar.gz"
+  sha256 "7ea654afe4879e0e9da449a1d039f2afff0a09c105c57d76e20a75aed3370d1b"
+
+  head do
+    url "https://github.com/nao20010128nao/void.git"
+  end
+
+  bottle :unneeded
+
+  def install
+    system "wget", "https://nao20010128nao.github.io/ytdl-patched/youtube-dl.tar.gz" if build.head?
+    system "tar", "-xzvf", "youtube-dl.tar.gz", "--strip-components=1" if build.head?
+    bin.install "youtube-dl"
+  end
+
+  test do
+    system "#{bin}/youtube-dl", "--simulate", "https://www.youtube.com/watch?v=he2a4xK8ctk"
+    system "#{bin}/youtube-dl", "--simulate", "--yes-playlist", "https://www.youtube.com/watch?v=iCkYw3cRwLo&list=LLnHXLLNHjNAnDQ50JANLG1g"
+  end
+end
