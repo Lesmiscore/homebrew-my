49e26f9e3313fe2f156e36f0757645fcc8836a0b
nao20010128nao <nao20010128@gmail.com>
2019-08-20 03:22:15 +0000

    ytdl-patched: 1566271260

--- Formula/ytdl-patched.rb
+++ Formula/ytdl-patched.rb
@@ -3,7 +3,7 @@ class YtdlPatched < Formula
   homepage "https://ytdl-org.github.io/youtube-dl/"
   url "https://nao20010128nao.github.io/ytdl-patched/youtube-dl.tar.gz"
   sha256 ""
-  version "1566228858"
+  version "1566271260"
 
   head do
     url "https://github.com/nao20010128nao/void.git"
