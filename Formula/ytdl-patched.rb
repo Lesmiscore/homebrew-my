3f832b9e9bd50007777309783a290a84a8f65bc2
nao20010128nao <nao20010128@gmail.com>
2021-01-18 02:45:43 +0000

    Revert "update formula to do same as youtube-dl formula do"
    This reverts commit faa2f347c7617d86bc57e89d2b59444a26d398c7.

--- Formula/ytdl-patched.rb
+++ Formula/ytdl-patched.rb
@@ -1,12 +1,9 @@
 class YtdlPatched < Formula
-  include Language::Python::Virtualenv
- 
   desc "Download YouTube videos from the command-line"
   homepage "https://ytdl-org.github.io/youtube-dl/"
   url "https://github.com/nao20010128nao/ytdl-patched/releases/download/1610908868/youtube-dl.tar.gz"
   sha256 "143d758b02d351879302d2abf14f7ade2e4de2ccc2803ef097c38682726dc1e3"
   version "1610908868"
-  license "Unlicense"
 
   head do
     url "https://github.com/nao20010128nao/ytdl-patched/releases/download/1610908868/youtube-dl.tar.gz"
@@ -15,11 +12,13 @@ class YtdlPatched < Formula
   bottle :unneeded
 
   def install
-    virtualenv_install_with_resources
+    system "wget", "https://nao20010128nao.github.io/ytdl-patched/youtube-dl.tar.gz" if build.head?
+    system "tar", "-xzvf", "youtube-dl.tar.gz", "--strip-components=1" if build.head?
+    bin.install "youtube-dl"
   end
 
   test do
-    system "#{bin}/youtube-dl", "--simulate", "https://www.youtube.com/watch?v=pOtd1cbOP7k"
-    system "#{bin}/youtube-dl", "--simulate", "--yes-playlist", "https://www.youtube.com/watch?v=pOtd1cbOP7k&list=PLMsZ739TZDoLj9u_nob8jBKSC-mZb0Nhj"
+    system "#{bin}/youtube-dl", "--simulate", "https://www.youtube.com/watch?v=he2a4xK8ctk"
+    system "#{bin}/youtube-dl", "--simulate", "--yes-playlist", "https://www.youtube.com/watch?v=iCkYw3cRwLo&list=LLnHXLLNHjNAnDQ50JANLG1g"
   end
 end
