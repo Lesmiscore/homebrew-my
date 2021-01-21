491535a6acd90ce56810d960d9f8f6790adaca3f
nao20010128nao <nao20010128@gmail.com>
2021-01-21 18:46:08 +0000

    ytdl-patched: 1611254540

--- Formula/ytdl-patched.rb
+++ Formula/ytdl-patched.rb
@@ -1,12 +1,12 @@
 class YtdlPatched < Formula
   desc "Download YouTube videos from the command-line"
   homepage "https://ytdl-org.github.io/youtube-dl/"
-  url "https://github.com/nao20010128nao/ytdl-patched/releases/download/1611192445/youtube-dl.tar.gz"
-  sha256 "b54e860c857433e51fd0c127959817b5826e5cbccb059de7d1bbcff4f44d258c"
-  version "1611192445"
+  url "https://github.com/nao20010128nao/ytdl-patched/releases/download/1611254540/youtube-dl.tar.gz"
+  sha256 "ac9a1fa4d8c4947a578b5a04ec0a4a467f35c7c63c8798dd056852fb1305d42b"
+  version "1611254540"
 
   head do
-    url "https://github.com/nao20010128nao/ytdl-patched/releases/download/1611192445/youtube-dl.tar.gz"
+    url "https://github.com/nao20010128nao/ytdl-patched/releases/download/1611254540/youtube-dl.tar.gz"
   end
 
   bottle :unneeded
