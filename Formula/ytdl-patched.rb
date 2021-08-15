a83aea4c269e1f0c5d32e89c2ebac26eba768c2b
The Hatsune Daishi <nao20010128@gmail.com>
2021-08-15 11:39:21 +0900

    Update ytdl-patched.rb

--- Formula/ytdl-patched.rb
+++ Formula/ytdl-patched.rb
@@ -3,7 +3,7 @@ class YtdlPatched < Formula
 
   desc "Download YouTube videos from the command-line"
   homepage "https://github.com/ytdl-patched/ytdl-patched"
-  url "https://github.com/ytdl-patched/ytdl-patched/releases/download/1628994378/youtube-dl.tar.gz"
+  url "https://github.com/ytdl-patched/ytdl-patched/releases/download/1628994378/yt_dlp-2021.08.15.1628994378-wheel.tar.gz"
   version "1628994378"
   sha256 "34c496dee19a59d5f09be216e92498fb70f1f675ddfb130194ac0cd64f8c2f3c"
   license "Unlicense"
