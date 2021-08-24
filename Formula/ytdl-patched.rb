d58da6f3f81b26b72fc1af2bc0958b57e587382c
nao20010128nao <nao20010128@gmail.com>
2021-08-24 01:51:02 +0000

    ytdl-patched: 1629769269

--- Formula/ytdl-patched.rb
+++ Formula/ytdl-patched.rb
@@ -3,9 +3,9 @@ class YtdlPatched < Formula
 
   desc "Download YouTube videos from the command-line"
   homepage "https://github.com/ytdl-patched/ytdl-patched"
-  url "https://github.com/ytdl-patched/ytdl-patched/releases/download/1629768540/yt_dlp-2021.08.24.1629768540-wheel.tar.gz"
-  version "1629768540"
-  sha256 "8a5c91e51f6bfa2213446a4cc37bb237d848afa19b7b6121c0f75215a5d7fc44"
+  url "https://github.com/ytdl-patched/ytdl-patched/releases/download/1629769269/yt_dlp-2021.08.24.1629769269-wheel.tar.gz"
+  version "1629769269"
+  sha256 "6516b4f29f86d6738de3b4cd78fd5e932fedce73d8670e2ffa2f5ba3f237e993"
   license "Unlicense"
 
   bottle :unneeded
