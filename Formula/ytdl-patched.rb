be43dce6b905ac83cbed5dfeb6cff6dc3fc397de
nao20010128nao <nao20010128@gmail.com>
2021-10-05 03:00:08 +0000

    ytdl-patched: 1633402306

--- Formula/ytdl-patched.rb
+++ Formula/ytdl-patched.rb
@@ -3,9 +3,9 @@ class YtdlPatched < Formula
 
   desc "youtube-dl fork fork fork with some more features"
   homepage "https://github.com/ytdl-patched/ytdl-patched"
-  url "https://github.com/ytdl-patched/ytdl-patched/releases/download/1633398696/yt_dlp-wheel.tar.gz"
-  version "1633398696"
-  sha256 "4c19d3edbc0df43fd723c34acac50c409ae7aa9d8f9b0bd69295cf4c3c140bef"
+  url "https://github.com/ytdl-patched/ytdl-patched/releases/download/1633402306/yt_dlp-wheel.tar.gz"
+  version "1633402306"
+  sha256 "20955dfd95ae2daa13264a5d02674d7244ec11f796f901632474822589b57312"
   license "Unlicense"
 
   bottle :unneeded
