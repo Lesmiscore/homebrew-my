f40392e5ad272325bdbfa9fd3ab7a7cd6a863ca3
nao20010128nao <nao20010128@gmail.com>
2021-10-18 09:54:59 +0000

    ytdl-patched: 1634550488

--- Formula/ytdl-patched.rb
+++ Formula/ytdl-patched.rb
@@ -3,9 +3,9 @@ class YtdlPatched < Formula
 
   desc "youtube-dl fork fork fork with some more features"
   homepage "https://github.com/ytdl-patched/ytdl-patched"
-  url "https://github.com/ytdl-patched/ytdl-patched/releases/download/1634537847/yt_dlp-wheel.tar.gz"
-  version "1634537847"
-  sha256 "968ab579752f9879d751305a707e14cc0c41e1efa98c37ba1f3d791515c5a9a0"
+  url "https://github.com/ytdl-patched/ytdl-patched/releases/download/1634550488/yt_dlp-wheel.tar.gz"
+  version "1634550488"
+  sha256 "520948ef8eb9399507885a283346b7d50539821115a3c1aad6c052534b9728ef"
   license "Unlicense"
 
   bottle :unneeded
@@ -13,18 +13,18 @@ class YtdlPatched < Formula
   depends_on "python@3.9"
 
   resource "mutagen" do
-    url "https://files.pythonhosted.org/packages/f3/d9/2232a4cb9a98e2d2501f7e58d193bc49c956ef23756d7423ba1bd87e386d/mutagen-1.45.1.tar.gz"
-    sha256 "6397602efb3c2d7baebd2166ed85731ae1c1d475abca22090b7141ff5034b3e1"
+    url "https://github.com/ytdl-patched/ytdl-patched/releases/download/1634550488/yt_dlp-wheel.tar.gz"
+    sha256 "520948ef8eb9399507885a283346b7d50539821115a3c1aad6c052534b9728ef"
   end
 
   resource "pycryptodomex" do
-    url "https://files.pythonhosted.org/packages/47/14/dd9ad29cd29ea4cc521286f2cb401ca7ac6fd5db0791c5e9bacaf2c9ac78/pycryptodomex-3.11.0.tar.gz"
-    sha256 "0398366656bb55ebdb1d1d493a7175fc48ade449283086db254ac44c7d318d6d"
+    url "https://github.com/ytdl-patched/ytdl-patched/releases/download/1634550488/yt_dlp-wheel.tar.gz"
+    sha256 "520948ef8eb9399507885a283346b7d50539821115a3c1aad6c052534b9728ef"
   end
 
   resource "websockets" do
-    url "https://files.pythonhosted.org/packages/1c/f4/61aee1eb4baadf8477fb7f3bc6b04a50fe683ef8ad2f60282806821e4b3b/websockets-10.0.tar.gz"
-    sha256 "c4fc9a1d242317892590abe5b61a9127f1a61740477bfb121743f290b8054002"
+    url "https://github.com/ytdl-patched/ytdl-patched/releases/download/1634550488/yt_dlp-wheel.tar.gz"
+    sha256 "520948ef8eb9399507885a283346b7d50539821115a3c1aad6c052534b9728ef"
   end
 
   def install
