df14f27fcec214058909c37d516fe6f65cd29b12
lesmiscore <10355528+Lesmiscore@users.noreply.github.com>
2023-04-21 05:09:12 +0000

    ytdl-patched: 2023.04.21.1682053209

--- Formula/ytdl-patched.rb
+++ Formula/ytdl-patched.rb
@@ -3,9 +3,9 @@ class YtdlPatched < Formula
 
   desc "youtube-dl fork fork fork with some more features"
   homepage "https://github.com/ytdl-patched/ytdl-patched"
-  url "https://github.com/ytdl-patched/ytdl-patched/releases/download/2023.04.19.1681864704/yt_dlp-wheel.tar.gz" # replace-marker
-  version "2023.04.19.1681864704" # replace-marker
-  sha256 "91b8ad0a662398091240fddaee44ae299f9b517cf0b0d0229ddceeb4f6a2ae98" # replace-marker
+  url "https://github.com/ytdl-patched/ytdl-patched/releases/download/2023.04.21.1682053209/yt_dlp-wheel.tar.gz" # replace-marker
+  version "2023.04.21.1682053209" # replace-marker
+  sha256 "13120b2f1310ed97e2f11149185678bb6f0554d047b64576fdb9d4ca1df896c0" # replace-marker
   license "Unlicense"
 
   depends_on "python@3.10"
